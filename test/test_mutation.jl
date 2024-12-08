7fb91cdde6e7c55269734e9f2cd6f7b5492b0c0c
Dominik S. <dsiebelt@campus.tu-berlin.de>
2024-12-08 17:48:03 +0100

    Add call function to mutation type

--- test/test_mutation.jl
+++ test/test_mutation.jl
@@ -1,5 +1,19 @@
 using Test
+using GeneticAlgorithms.Mutation
 
 include("../src/GeneticAlgorithms.jl")
 
-# TODO
+
+@testset "Mutation.jl" begin
+
+    mutation = RealGeneMutation(0.1, (-0.5, 0.5))
+    mutated_genes = mutation([0.0, 0.0, 0.0, 0.0, 0.0])
+    @test length(mutated_genes) == 5
+    for gene in mutated_genes
+        @test -0.5 <= gene <= 0.5
+    end
+    @test mutation.mutation_rate == 0.1
+    @test mutation.mutation_interval == (-0.5, 0.5)
+end
+
+
